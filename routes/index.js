795c37cbb9d6bf9d636ca1e88331d6bd51f5e0f6
koffiedy <koffiedy@gmail.com>
2020-02-18 17:34:33 +0000

    clubs ajour et ajout du formulaire entreprise

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,7 @@ var cloudant = Cloudant("https://042093a3-40d2-45d8-b0d5-25696e87b30f-bluemix:2e
 })
 var cid_projets = cloudant.use('cid_projets')
 var cid_responsables = cloudant.use('cid_responsables')
-const clubs = data.clubs
-const profs = data.profs
-
+const cid_clubs = cloudant.use('cid_clubs')
 
 router.get('/', function (req, res, next) {
     let projets = {
@@ -28,30 +26,49 @@ router.get('/', function (req, res, next) {
             }
         }
     }
+    let clubs = {
+        selector: {
+            _id: {
+                "$gt": null
+            }
+        }
+    }
     cid_projets.find(projets).then((projets) => {
         cid_responsables.find(profs).then((profs) => {
-            res.render('accueil/index', { title: 'CID | Accueil', projets: projets.docs, clubs: clubs, profs: profs.docs, host: req.hostname });
+            cid_clubs.find(clubs).then((clubs) => {
+                res.render('accueil/index', { title: 'CID | Accueil', projets: projets.docs, clubs: clubs.docs, profs: profs.docs, host: req.hostname });
+            })
         })
     })
 
 
 });
 
-router.get('/entreprise', function (req, res, next) {
-    res.render('accueil/entreprise', { title: 'CID | Accueil', host: req.hostname });
+router.get('/clubs/:id', function (req, res, next) {
+    let club = {
+        selector: {
+            _id: {
+                "$eq": req.params.id
+            }
+        }
+    }
+    cid_clubs.find(club).then((club) => {
+        console.log(club)
+        res.render('accueil/club_details', { title: 'CID | Club', club: club.docs[0], host: req.hostname });
+    })
 });
+
 router.get('/projets/:id', function (req, res, next) {
     let projet = {
         selector: {
             _id: {
                 "$eq": req.params.id
-
             }
         }
     }
     cid_projets.find(projet).then((projet) => {
         console.log(projet)
-        res.render('accueil/projet_details', { title: 'CID | Accueil', projet: projet.docs[0], host: req.hostname });
+        res.render('accueil/projet_details', { title: 'CID | Projets', projet: projet.docs[0], host: req.hostname });
     })
 });
 
